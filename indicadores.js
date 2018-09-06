d527db1d819f0eef8d8ff1b57ba1445e6e87ea23
ramonbarco <reimonfinger@gmail.com>
2018-09-06 17:23:42 -0300

    update gráficos

--- indicadores.js
+++ indicadores.js
@@ -1,55 +1,46 @@
 export const data = {
-  PersonaldeInvestigacion: [
-    ['Task', 'Hours per Day'],
-    ['Categoría 1', 300],
-    ['Categoría 2', 150]
-  ],
+  PersonaldeInvestigacion: [['Categoría', 'Personas'], ['1', 300], ['2', 150]],
   categorizados: [
-    ['Task', 'Categorizados año 2009 - 2011 - 2014'],
-    ['Categoría 1', 300],
-    ['Categoría 2', 150],
-    ['Categoría 3', 110],
-    ['Categoría 4', 23],
-    ['Categoría 5', 98]
+    ['Categoría', 'Investigadores'],
+    ['I', 300],
+    ['II', 150],
+    ['III', 110],
+    ['IV', 23],
+    ['V', 98]
   ],
   opcionescategorizados: {
-    chart: {
-      title: 'categorizados'
-    },
+    title: 'categorizados',
     colors: ['#1b9e77', '#d95f02', '#7570b3'],
     pieHole: 0.2,
-    height: 150,
-    width: 450
-  },
-  opcionescategorizados: {
-    chart: {
-      title: 'categorizados'
-    },
-    colors: ['#1b9e77', '#d95f02', '#7570b3', '#bfd817', '#d81327'],
-    pieHole: 0.2,
-    height: 250,
-    width: 450
+    height: 200,
+    width: 450,
+    title: 'Categorizados 2009-2011-2014'
   },
   investigadoressegundedicacion: [
-    ['Task', 'Investigadores según cargo'],
+    ['Dedicacion docente', 'Investigadores según cargo'],
     ['Exclusiva', 300],
     ['Semiexclusiva', 150],
     ['Simple', 110]
   ],
   opcionesinvestigadoressegundedicacion: {
-    chart: {
-      title: 'Investigadores según cargo'
-    },
+    title: 'Investigadores según cargo',
     colors: ['#1b9e77', '#d95f02', '#7570b3'],
     height: 150,
     width: 450
   },
-  opcionesinvestigadoressegundedicacion: {
-    chart: {
-      title: 'Investigadores según cargo'
-    },
+  distribucionDeInvestigadoresBecariosyCpaTotalSegunUa: [
+    ['UA', 'Investigadores', 'Becarios'],
+    ['ECYT', 300, 20],
+    ['IIB', 150, 5],
+    ['EPYG', 110, 100],
+    ['EH', 120, 10]
+  ],
+  opcionesDistribucionDeInvestigadoresBecariosyCpaTotalSegunUa: {
+    title: 'Distribución Investigadores,becarios y CPA por Unidad Académica',
     colors: ['#1b9e77', '#d95f02', '#7570b3'],
     height: 150,
-    width: 450
+    width: 450,
+    bar: { groupWidth: '35%' },
+    isStacked: true
   }
 }
